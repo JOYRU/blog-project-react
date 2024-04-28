3f4d370a70a7e554a45d9b73496e1a895940e384
JOYRU <joycseru@gmail.com>
2024-04-28 21:46:19 +0600

    use html code in js file

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+
+//     <h1>
+//       Hello
+//     </h1>
+//   </React.StrictMode>,
+// )
+const todo = "Todo Aoopp" ; 
+const date = new Date()  ; 
+const currentYear = date.getFullYear() ; 
+ReactDOM.render(
+  <div>
+    <h1 className='headingStyle'>{todo}</h1>
+    <div className='card'>
+         <h3 className='cardTitle'>Call Family</h3>
+         <p className='cardDesc'>Hey, Please Call to everyone</p>
+         {/* <p className='cardFooter'>{date}</p> */}
+    </div>
+  </div>
+   , 
+document.getElementById('root')
+
+) ; 
